67b8473fd26964b17d18b90ce1ec168227cc9f66
austinried <4966622+austinried@users.noreply.github.com>
2024-11-30 13:38:39 +0900

    bring tab completion back

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -28,27 +28,27 @@
     #   "ignorespace"
     # ];
 
-    # initExtra = ''
-    #   # If there are multiple matches for completion, Tab should cycle through them
-    #   bind 'TAB:menu-complete'
-    #   # And Shift-Tab should cycle backwards
-    #   bind '"\e[Z": menu-complete-backward'
+    initExtra = ''
+      # If there are multiple matches for completion, Tab should cycle through them
+      bind 'TAB:menu-complete'
+      # And Shift-Tab should cycle backwards
+      bind '"\e[Z": menu-complete-backward'
 
-    #   # Display a list of the matching files
-    #   bind "set show-all-if-ambiguous on"
+      # Display a list of the matching files
+      bind "set show-all-if-ambiguous on"
 
-    #   # Perform partial (common) completion on the first Tab press, only start
-    #   # cycling full results on the second Tab press (from bash version 5)
-    #   bind "set menu-complete-display-prefix on"
+      # Perform partial (common) completion on the first Tab press, only start
+      # cycling full results on the second Tab press (from bash version 5)
+      bind "set menu-complete-display-prefix on"
 
-    #   # Cycle through history based on characters already typed on the line
-    #   bind '"\e[A":history-search-backward'
-    #   bind '"\e[B":history-search-forward'
+      # Cycle through history based on characters already typed on the line
+      # bind '"\e[A":history-search-backward'
+      # bind '"\e[B":history-search-forward'
 
-    #   # Keep Ctrl-Left and Ctrl-Right working when the above are used
-    #   bind '"\e[1;5C":forward-word'
-    #   bind '"\e[1;5D":backward-word'
-    # '';
+      # Keep Ctrl-Left and Ctrl-Right working when the above are used
+      bind '"\e[1;5C":forward-word'
+      bind '"\e[1;5D":backward-word'
+    '';
   };
 
   programs.atuin = {
@@ -56,6 +56,7 @@
     # https://docs.atuin.sh/configuration/config/
     settings = {
       style = "compact";
+      inline_height = 12;
       filter_mode_shell_up_key_binding = "session";
     };
   };
