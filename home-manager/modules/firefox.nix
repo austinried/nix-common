427a19e715a5810e47bf8423d6a85db532456c9e
austinried <4966622+austinried@users.noreply.github.com>
2024-12-14 18:19:05 +0900

    disable overscroll in firefox

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -5,7 +5,6 @@
 
     profiles.default = {
       settings = {
-        # "browser.startup.homepage" = "https://duckduckgo.com";
         "browser.search.defaultenginename" = "DuckDuckGo";
         "browser.search.order.1" = "DuckDuckGo";
 
@@ -13,16 +12,8 @@
         "widget.use-xdg-desktop-portal.file-picker" = 1;
         "browser.aboutConfig.showWarning" = false;
         "browser.compactmode.show" = true;
-        "browser.cache.disk.enable" = false; # Be kind to hard drive
 
-        # "mousewheel.default.delta_multiplier_x" = 20;
-        # "mousewheel.default.delta_multiplier_y" = 20;
-        # "mousewheel.default.delta_multiplier_z" = 20;
-
-        # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
-        # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
-        # then have Firefox open on some other workspace.
-        # "widget.disable-workspace-management" = true;
+        "apz.overscroll.enabled" = false;
       };
       search = {
         force = true;
