22f30154dd6373f1344fe5a73aee61589f9caad5
austinried <4966622+austinried@users.noreply.github.com>
2024-12-14 04:50:53 +0000

    nix tools, more gnome settings

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,11 @@
-{ pkgs, lib, isWorkstation, stateVersion, ... }:
+{
+  pkgs,
+  pkgs-unfree,
+  lib,
+  isWorkstation,
+  stateVersion,
+  ...
+}:
 {
   imports = [ ./modules ];
 
@@ -58,64 +65,90 @@
       style = "compact";
       inline_height = 12;
       filter_mode_shell_up_key_binding = "session";
+      enter_accept = true;
     };
   };
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
 
-  home.packages = with pkgs; []
-  ++ lib.optionals isWorkstation [
-    eyedropper
-    gnome-extension-manager
-    papirus-icon-theme
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  home.packages =
+    with pkgs;
+    [ ]
+    ++ lib.optionals isWorkstation [
+      eyedropper
+      gnome-extension-manager
+      papirus-icon-theme
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
-    gnome-tweaks
-    gnomeExtensions.appindicator
-    gnomeExtensions.emoji-copy
-    gnomeExtensions.just-perfection
-    gnomeExtensions.wireless-hid
-    gnomeExtensions.wifi-qrcode
-    gnomeExtensions.vitals
-    gnomeExtensions.clipboard-history
-  ];
+      gnome-tweaks
+      gnomeExtensions.appindicator
+      gnomeExtensions.emoji-copy
+      gnomeExtensions.just-perfection
+      gnomeExtensions.wireless-hid
+      gnomeExtensions.wifi-qrcode
+      gnomeExtensions.vitals
+      gnomeExtensions.clipboard-history
+
+      pkgs-unfree.discord
+
+      nixd
+      nil
+      nixfmt-rfc-style
+      nix-direnv
+    ];
 
   # https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/
   # https://github.com/nix-community/dconf2nix
-  dconf.settings = lib.mkIf isWorkstation (with lib.hm.gvariant; {
-    "org/gnome/desktop/interface" = {
-      clock-format = "24h";
-      clock-show-weekday = true;
-      enable-hot-corners = true;
-      show-battery-percentage = true;
-      enable-animations = true;
-      color-scheme = "prefer-dark";
-      icon-theme = "Papirus-Dark";
-      font-name = "Cantarell 12";
-      document-font-name = "Cantarell 12";
-      monospace-font-name = "JetBrainsMonoNL Nerd Font Mono 11";
-      font-antialiasing = "rgba";
-      font-hinting = "slight";
-    };
-    "org/gnome/desktop/wm/preferences" = {
-      button-layout = "appmenu:minimize,maximize,close";
-    };
-    "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = [
-        "appindicatorsupport@rgcjonas.gmail.com"
-        "drive-menu@gnome-shell-extensions.gcampax.github.com"
-        "user-theme@gnome-shell-extensions.gcampax.github.com"
-        "wifiqrcode@glerro.pm.me"
-        "wireless-hid@chlumskyvaclav.gmail.com"
-        "just-perfection-desktop@just-perfection"
-        "clipboard-history@alexsaveau.dev"
-      ];
-    };
-    # TODO: just perfection settings
-    "org/gnome/shell/extensions/just-perfection" = {};
-  });
+  dconf.settings = lib.mkIf isWorkstation (
+    with lib.hm.gvariant;
+    {
+      "org/gnome/desktop/interface" = {
+        clock-format = "24h";
+        clock-show-weekday = true;
+        enable-hot-corners = true;
+        show-battery-percentage = true;
+        enable-animations = true;
+        color-scheme = "prefer-dark";
+        icon-theme = "Papirus-Dark";
+        font-name = "Cantarell 11";
+        document-font-name = "Cantarell 12";
+        monospace-font-name = "JetBrainsMonoNL Nerd Font Mono 11";
+        font-antialiasing = "rgba";
+        font-hinting = "slight";
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          "appindicatorsupport@rgcjonas.gmail.com"
+          "drive-menu@gnome-shell-extensions.gcampax.github.com"
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+          "wifiqrcode@glerro.pm.me"
+          "wireless-hid@chlumskyvaclav.gmail.com"
+          "just-perfection-desktop@just-perfection"
+          "clipboard-history@alexsaveau.dev"
+        ];
+      };
+      "org/gnome/shell/extensions/just-perfection" = {
+        quick-settings-dark-mode = false;
+        switcher-popup-delay = false;
+      };
+      "org/gnome/desktop/wm/keybindings" = {
+        # Alt+Tab windows not grouped applications
+        switch-applications = [ ];
+        switch-applications-backward = [ ];
+        switch-windows = [ "<Alt>Tab" ];
+        switch-windows-backwards = [ "<Shift><Alt>Tab" ];
+      };
+
+      # Rebind Super+v so it can be used for history paste
+      "org/gnome/desktop/wm/keybindings".toggle-message-tray = [ ];
+      "org/gnome/shell/extensions/clipboard-history".toggle-menu = [ "<Super>v" ];
+    }
+  );
 
   home.stateVersion = stateVersion;
 }
