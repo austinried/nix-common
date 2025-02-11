87ae54a8d7ab8ef706d2eda0ccf03224706fae9b
austinried <4966622+austinried@users.noreply.github.com>
2025-02-11 17:54:14 +0900

    split gnome into hm/nixos modules

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./shell.nix
+    ./gnome.nix
     ./developer
   ];
 }
--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -1,16 +1,15 @@
 {
   lib,
   config,
-  pkgs,
-  username,
   ...
 }:
 let
-  module = "gnome";
-  cfg = config.common.${module};
+  cfg = config.common.gnome;
 in
 {
-  options.common.${module}.enable = lib.mkEnableOption "Enable the Gnome desktop environment.";
+  options.common.gnome = {
+    enable = lib.mkEnableOption "Enable the Gnome desktop environment.";
+  };
 
   config = lib.mkIf cfg.enable {
     services.xserver.enable = true;
@@ -26,110 +25,5 @@ in
       enable = true;
       pulse.enable = true;
     };
-
-    fonts.packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-    ];
-
-    home-manager.users.${username} =
-      {
-        lib,
-        pkgs,
-        ...
-      }:
-      {
-        home.packages = with pkgs; [
-          ghostty
-
-          eyedropper
-          gnome-extension-manager
-          papirus-icon-theme
-
-          gnome-tweaks
-          gnomeExtensions.appindicator
-          gnomeExtensions.emoji-copy
-          gnomeExtensions.just-perfection
-          gnomeExtensions.wireless-hid
-          gnomeExtensions.wifi-qrcode
-          gnomeExtensions.vitals
-          gnomeExtensions.clipboard-history
-
-          vlc
-
-          ungoogled-chromium
-          thunderbird-latest
-
-          impression
-        ];
-
-        # https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/
-        # https://github.com/nix-community/dconf2nix
-        # https://nix-community.github.io/home-manager/index.xhtml#sec-option-types
-        dconf.settings = with lib.hm.gvariant; {
-          "org/gnome/desktop/interface" = {
-            clock-format = "24h";
-            clock-show-weekday = true;
-            enable-hot-corners = true;
-            show-battery-percentage = true;
-            enable-animations = true;
-            color-scheme = "prefer-dark";
-            icon-theme = "Papirus-Dark";
-            font-name = "Cantarell 11";
-            document-font-name = "Cantarell 12";
-            monospace-font-name = "JetBrainsMonoNL Nerd Font Mono 11";
-            font-antialiasing = "rgba";
-            font-hinting = "slight";
-          };
-          "org/gnome/desktop/wm/preferences" = {
-            button-layout = "appmenu:minimize,maximize,close";
-          };
-          "org/gnome/shell" = {
-            disable-user-extensions = false;
-            enabled-extensions = [
-              "appindicatorsupport@rgcjonas.gmail.com"
-              "drive-menu@gnome-shell-extensions.gcampax.github.com"
-              "user-theme@gnome-shell-extensions.gcampax.github.com"
-              "wifiqrcode@glerro.pm.me"
-              "wireless-hid@chlumskyvaclav.gmail.com"
-              "just-perfection-desktop@just-perfection"
-              "clipboard-history@alexsaveau.dev"
-            ];
-          };
-          "org/gnome/shell/extensions/just-perfection" = {
-            quick-settings-dark-mode = false;
-            switcher-popup-delay = false;
-          };
-
-          "org/gnome/shell".favorite-apps = [
-            "org.gnome.Nautilus.desktop"
-            "com.mitchellh.ghostty.desktop"
-            "firefox.desktop"
-            "codium.desktop"
-          ];
-
-          "org/gnome/desktop/wm/keybindings" = {
-            # Alt+Tab windows not grouped applications
-            switch-applications = [ ];
-            switch-applications-backward = [ ];
-            switch-windows = [ "<Alt>Tab" ];
-            switch-windows-backwards = [ "<Shift><Alt>Tab" ];
-          };
-
-          # Rebind Super+v so it can be used for history paste
-          "org/gnome/desktop/wm/keybindings".toggle-message-tray = [ ];
-          "org/gnome/shell/extensions/clipboard-history".toggle-menu = [ "<Super>v" ];
-
-          "org/gnome/settings-daemon/plugins/media-keys".home = [ "<Super>e" ];
-
-          "org/gnome/mutter" = {
-            edge-tiling = true;
-            dynamic-workspaces = true;
-
-            # Prevents "X is not responding" prompts
-            check-alive-timeout = mkUint32 0;
-          };
-          "org/gnome/shell/app-switcher".current-workspace-only = true;
-        };
-      };
   };
 }
