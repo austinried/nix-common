8c74580cc2afc60cd00af3a31b81de4851a01773
austinried <4966622+austinried@users.noreply.github.com>
2025-02-12 17:41:48 +0900

    add tilix and config, and blackbox for trial

--- home-manager/modules/terminal.nix
+++ home-manager/modules/terminal.nix
@@ -14,10 +14,38 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
+      tilix
+      blackbox-terminal
+
       (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
 
     fonts.fontconfig.enable = true;
+
+    dconf.settings = with lib.hm.gvariant; {
+      "com/gexperts/Tilix" = {
+        prompt-on-close = true;
+        sidebar-on-right = false;
+        tab-position = "top";
+        terminal-title-show-when-single = true;
+        terminal-title-style = "small";
+        theme-variant = "system";
+        use-tabs = true;
+        window-style = "borderless";
+      };
+
+      "com/gexperts/Tilix/profiles" = {
+        default = "2b7c4080-0ddd-46c5-8f23-563fd3ba789d";
+        list = [ "2b7c4080-0ddd-46c5-8f23-563fd3ba789d" ];
+      };
+
+      "com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d" = {
+        visible-name = "Default";
+        font = "JetBrainsMonoNL Nerd Font Mono 11";
+        use-system-font = false;
+        terminal-title = "\${id}: \${title}";
+      };
+    };
 
     programs.ghostty = {
       enable = true;
